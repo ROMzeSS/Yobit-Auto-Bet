b73f561b102e0d509e28429315a2f6822adcfbe2
Hưng Việt <hungnvz@users.noreply.github.com>
2017-07-21 14:39:35 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,63 @@
+var min_bet = 0.00000100,
+    current_bet = min_bet,
+    lose_count = 0;
+
+var betting = false;
+
+function start() {
+
+    if (betting)
+        return;
+    betting = true;
+    var balance = getBalance();
+    if (balance < current_bet) {
+        alert('Empty balance !');
+        return;
+    }
+
+    // Set bet amount
+    $('.dice_htable input[name=bet]').val(parseFloat(current_bet).toFixed(8));
+
+    // Random roll
+    var roll = Math.floor(Math.random() * 2);
+    var dicePlayBtn = $('.dice_htable .clDicePlay')[roll];
+    dicePlayBtn.click();
+
+    checkResult(balance);
+}
+
+function checkResult(last_balance) {
+    setTimeout(function() {
+        if (last_balance < getBalance()) {
+            // Win
+            current_bet = min_bet;
+            lose_count = 0;
+            betting = false;
+        } else if (last_balance > getBalance()) {
+            // Lose
+            if (++lose_count > 10) {
+                current_bet = min_bet;
+                lose_count = 0;
+                betting = false;
+            } else {
+                current_bet = current_bet * 2;
+                betting = false;
+            }
+        } else {
+            checkResult(last_balance);
+        }
+    }, 500);
+}
+
+function getBalance() {
+    var dice_text = $('.dice_select .chosen-single span').text();
+
+    var regex = /[+-]?\d+(\.\d+)?/g;
+    var floats = dice_text.match(regex).map(function(v) {
+        return parseFloat(v).toFixed(8);
+    });
+
+    return floats[0];
+}
+
+setInterval(start, 100);
